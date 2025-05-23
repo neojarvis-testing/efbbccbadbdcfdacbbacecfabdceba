5fa122b0d71aa2acb2f338b4e9636b0ec6a64c8f
neojarvis <techadmin@iamneo.ai>
2025-05-23 17:12:54 +0000

    initialCommit-Boilerplate

---
+++ springappcare/src/main/java/com/examly/springappcare/SpringappcareApplication.java
@@ -0,0 +1,13 @@
+package com.examly.springappcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappcareApplication.class, args);
+	}
+
+}
