6ae11b551706a45d9695879608fe85ee455466ed
neojarvis <techadmin@iamneo.ai>
2025-05-27 03:47:28 +0000

    Auto-save-commit-5/27/2025--9:17:21-AM

--- springappuser/src/main/java/com/examly/springappuser/config/SecurityConfig.java
+++ springappuser/src/main/java/com/examly/springappuser/config/SecurityConfig.java
@@ -1,7 +1,36 @@
+import java.beans.BeanProperty;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-    
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> crsf.disable())
+        .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/users/login", "/api/users/register").permitAll()
+                    .anyRequest().authenticated()
+                    )
+                    .exceptionHandling(ex -> ex.authenticationEntryPoint(authenticationEntryPoint))
+                    .sessionManagement(session -> session.sessionCreationPolicy(sessionCreationPolicy.))
+                    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(Authentication)
+
+
     
 }
