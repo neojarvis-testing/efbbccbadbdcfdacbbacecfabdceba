669fb7d1c7dc9213239bf2ef2f4e309cfda1f67e
neojarvis <techadmin@iamneo.ai>
2025-05-27 04:17:28 +0000

    Auto-save-commit-5/27/2025--9:47:21-AM

--- springappuser/src/main/java/com/examly/springappuser/config/MyUserDetailsService.java
+++ springappuser/src/main/java/com/examly/springappuser/config/MyUserDetailsService.java
@@ -1,3 +1,17 @@
-public class MyUserDetailsService {
+package com.examly.springappuser.config;
+
+import com.examly.springappuser.config.UserPrinciple;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email)
+                     .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new UserPrinciple(user);
+    }
     
 }
--- springappuser/src/main/java/com/examly/springappuser/config/UserPrinciple.java
+++ springappuser/src/main/java/com/examly/springappuser/config/UserPrinciple.java
@@ -1,4 +1,29 @@
-package com
-public class UserPrinciple {
+package com.examly.springappuser.config;
+
+import java.util.Collection;
+
+public class UserPrinciple implements UserDetails{
+    private final User user;
+
+    public UserPrinciple(User user){
+        this.user = user;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + user.getUserRole()));
+    }
     
+    @Override
+    public String getPassword(){
+        return user.getPassword();
+    }
+    @Override
+    public String getUsername(){
+        return user.getEmail();
+    }
+    @Override
+    public boolean isAccountNonExpired() { return true;}
+    @Override
+    
+
 }
